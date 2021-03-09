e6f79d3c794c58f516d7bb06e9a7d50eddc6c5af
Roberto <r.attanasio90@gmail.com>
2021-03-09 10:18:37 +0100

    Menu Chevron "Lingua"
    Aggiunto chevron che cambia da up e down al passaggio del mouse

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    // COMPORTAMENTO MENU A TENDINA
     $('li.nav_item').each(function() {
         $(this).mouseover(function() {
             $(this).find('.nav_menu').addClass('active');
@@ -15,4 +17,30 @@ $(document).ready(function() {
             })
         })
     })
+
+    // COMPORTAMENTO PER CHEVRON UP/DOWN MENU
+    $('ul.language li.nav_item').each(function() {
+        $(this).mouseover(function() {
+            $(this).find('.nav_menu').addClass('active');
+            $(this).find('i').removeClass('fa-chevron-down');
+            $(this).find('i').addClass('fa-chevron-up');
+
+            $(this).find('.nav_menu').mouseover(function() {
+                $(this).find('.nav_menu').addClass('active');
+            })
+
+            $(this).mouseout(function() {
+                $(this).find('.nav_menu').removeClass('active');
+
+            })
+            $(this).find('.nav_menu').mouseout(function() {
+                $(this).removeClass('active');
+            })
+        })
+        $(this).mouseout(function() {
+            $(this).find('i').removeClass('fa-chevron-up');
+            $(this).find('i').addClass('fa-chevron-down');
+        })
+    })
+
 });
